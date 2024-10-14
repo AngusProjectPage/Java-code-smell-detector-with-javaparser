6a414073c10bc3f039e943f540d293447c7faafe
fmb21117 <angus.duffy.2021@uni.strath.ac.uk>
2024-10-14 15:16:31 +0100

    Simple Assignment Complete

--- Driver.java
+++ Driver.java
@@ -1,9 +1,9 @@
-
 import com.github.javaparser.StaticJavaParser;
 import com.github.javaparser.ast.CompilationUnit;
 import com.github.javaparser.ast.body.ClassOrInterfaceDeclaration;
 import com.github.javaparser.ast.body.FieldDeclaration;
 import com.github.javaparser.ast.body.VariableDeclarator;
+import com.github.javaparser.ast.expr.AssignExpr;
 import com.github.javaparser.ast.expr.VariableDeclarationExpr;
 import com.github.javaparser.ast.type.ClassOrInterfaceType;
 import com.github.javaparser.ast.visitor.VoidVisitorAdapter;
@@ -24,12 +24,30 @@ public class Driver {
     }
 
     public static class SmellyCodeVisitor extends VoidVisitorAdapter {
-        public void visit(ClassOrInterfaceDeclaration n, Object arg) {
 
+        @Override
+        public void visit(ClassOrInterfaceDeclaration n, Object args) {
+
+            super.visit(n, args);
         }
 
-        public void visit(FieldDeclaration n, Object a) {
+        @Override
+        public void visit(FieldDeclaration n, Object args) {
+            super.visit(n, args);
+        }
 
+        @Override
+        public void visit(AssignExpr n, Object args) {
+            simpleAssignment(n);
+            super.visit(n, args);
+        }
+
+        public void simpleAssignment(AssignExpr n) {
+            if(n.getValue().isAssignExpr()) SmellyCodeFound("Keep assignments simple " + n.getValue() + " cannot be assigned to " + n.getTarget());
+        }
+
+        public void SmellyCodeFound(String str) {
+            System.out.println(str);
         }
     }
 }
--- SmellyJava.java
+++ SmellyJava.java
@@ -1,31 +1,16 @@
 public class SmellyJava {
-
-    public static void main(String[] args) {
-        // Local variable declaration without initialization
+        int a, b, c;
         int counter;
-        counter = 1; // Initialized immediately after declaration, but not at the same time
-
-        // More bad practice: Variables are not initialized immediately, creating potential confusion
-        int a, b;
-        a = 'c';
-        b = 'c';
-
-        // Even worse: Making assignments unnecessarily complex
         int x;
         int y;
-        // Avoid assigning multiple variables to the same value in one statement
-        x = (y = 5); // Bad practice: assigning in nested manner
-
-        // Smelly logic using uninitialized or redundant assignments
         int smellyValue;
-        smellyValue = 0;  // Setting the value after declaring
-        smellyValue += 10;  // Incrementing in a less obvious manner
-
-        System.out.println("Counter: " + counter);
-        System.out.println("a: " + a);
-        System.out.println("b: " + b);
-        System.out.println("x: " + x);
-        System.out.println("y: " + y);
-        System.out.println("Smelly value: " + smellyValue);
-    }
+        public SmellyJava() {
+                counter = 1;
+                a = b = c = 'c';
+                x = (y = 5);
+                System.out.println(y);
+                System.out.println(x);
+                smellyValue += 10;
+                smellyValue = 0;
+        }
 }
