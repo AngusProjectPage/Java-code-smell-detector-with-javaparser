dbc99ad255d918eb6780070f49edd852f62b0c8a
Callum <callum.sergeant.2021@uni.strath.ac.uk>
2024-10-14 12:57:39 +0100

    Driver and some stinky code

--- Driver.java
+++ Driver.java
@@ -1,2 +1,26 @@
+
+import com.github.javaparser.StaticJavaParser;
+import com.github.javaparser.ast.CompilationUnit;
+import com.github.javaparser.ast.body.ClassOrInterfaceDeclaration;
+import com.github.javaparser.ast.body.FieldDeclaration;
+import com.github.javaparser.ast.body.VariableDeclarator;
+import com.github.javaparser.ast.expr.VariableDeclarationExpr;
+import com.github.javaparser.ast.type.ClassOrInterfaceType;
+import com.github.javaparser.ast.visitor.VoidVisitorAdapter;
+
+import java.io.FileInputStream;
+
+
 public class Driver {
+
+    public static void main(String[] args) throws Exception {
+        FileInputStream in = new FileInputStream("SmellyJava.java");
+
+        CompilationUnit cu;
+        try {
+            cu = StaticJavaParser.parse(in);
+        } finally {
+            in.close();
+        }
+    }
 }
