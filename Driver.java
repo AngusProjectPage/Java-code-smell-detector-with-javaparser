aef7f0b6e3169ae14f5f009f0014e1fa5aefc7eb
fmb21117 <angus.duffy.2021@uni.strath.ac.uk>
2024-10-14 13:06:42 +0100

    Base template complete

--- Driver.java
+++ Driver.java
@@ -10,17 +10,26 @@ import com.github.javaparser.ast.visitor.VoidVisitorAdapter;
 
 import java.io.FileInputStream;
 
-
 public class Driver {
 
     public static void main(String[] args) throws Exception {
         FileInputStream in = new FileInputStream("SmellyJava.java");
-
         CompilationUnit cu;
         try {
             cu = StaticJavaParser.parse(in);
         } finally {
             in.close();
+        }
+        new SmellyCodeVisitor().visit(cu, null);
+    }
+
+    public static class SmellyCodeVisitor extends VoidVisitorAdapter {
+        public void visit(ClassOrInterfaceDeclaration n, Object arg) {
+
+        }
+
+        public void visit(FieldDeclaration n, Object a) {
+
         }
     }
 }
