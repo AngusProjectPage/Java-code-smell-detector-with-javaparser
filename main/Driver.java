9b47786ea40b052c6a3c1e60e9c59751ba617214
fmb21117 <angus.duffy.2021@uni.strath.ac.uk>
2024-10-23 23:11:46 +0100

    Some extra work

--- main/Driver.java
+++ main/Driver.java
@@ -1,15 +1,22 @@
 package main;
 
+import com.github.javaparser.ast.body.ClassOrInterfaceDeclaration;
+import com.github.javaparser.ast.body.FieldDeclaration;
+import com.github.javaparser.ast.body.MethodDeclaration;
+import com.github.javaparser.ast.expr.AssignExpr;
+import com.github.javaparser.ast.expr.FieldAccessExpr;
 import main.Visitor.*;
 import main.dto.LimitAccessSmellDTO;
 import com.github.javaparser.StaticJavaParser;
 import com.github.javaparser.ast.CompilationUnit;
 import com.github.javaparser.ast.body.VariableDeclarator;
+import main.dto.OuterPrivateVariableExposedDTO;
+
 import java.io.FileInputStream;
 
 public class Driver {
     public static void main(String[] args) throws Exception {
-        FileInputStream in = new FileInputStream("C:\\dev\\mutableReferenceExposer.java");
+        FileInputStream in = new FileInputStream("C:\\dev\\multipleBadCodeInstances.java");
         CompilationUnit cu;
         try {
             cu = StaticJavaParser.parse(in);
@@ -22,7 +29,28 @@ public class Driver {
 //        new AvoidConstantsSmellVisitor().visit(cu, null);
 //        checkLimitAccessSmells(cu);
 //        new SwitchFallThroughSmellVisitor().visit(cu, null);
-        new PrivateMutableClassVariablesSmellVisitor().visit(cu, null);
+//        new PrivateMutableClassVariablesSmellVisitor().visit(cu, null);
+          checkOuterPrivateVariableExposedSmells(cu);
+    }
+
+    public static void checkOuterPrivateVariableExposedSmells(CompilationUnit cu) {
+        OuterPrivateVariableExposedDTO outerPrivateVariableExposedDTO = new OuterPrivateVariableExposedDTO();
+        new OuterPrivateVariableExposedSmellVisitor().visit(cu, outerPrivateVariableExposedDTO);
+        if(!outerPrivateVariableExposedDTO.getFieldDeclarations().isEmpty()) {
+            if(!outerPrivateVariableExposedDTO.getInnerClasses().isEmpty()) {
+                for(ClassOrInterfaceDeclaration ci : outerPrivateVariableExposedDTO.getInnerClasses()) {
+                    System.out.println(ci.getName() + " is an inner public class exposing private fields from outer class");
+                }
+            }
+            if(!outerPrivateVariableExposedDTO.getMethodDeclarations().isEmpty()) {
+                for(MethodDeclaration md : outerPrivateVariableExposedDTO.getMethodDeclarations()) {
+                    System.out.println(md.getName() + " is a public method exposing private fields in it's outer class");
+                }
+            }
+            for(VariableDeclarator vd : outerPrivateVariableExposedDTO.getFieldDeclarations()) {
+                System.out.println(vd + " is a private field being exposed by inner class");
+            }
+        }
     }
 
     public static void checkLimitAccessSmells(CompilationUnit cu) {
