b949a88606128e8ff1d437aa767b1e9b3104c612
Callum <callum.sergeant.2021@uni.strath.ac.uk>
2024-10-24 14:13:00 +0100

    5 and 9

--- main/Visitor/FieldDeclarationSmellVisitor.java
+++ main/Visitor/FieldDeclarationSmellVisitor.java
@@ -3,6 +3,7 @@ package main.Visitor;
 import com.github.javaparser.ast.Node;
 import com.github.javaparser.ast.body.FieldDeclaration;
 import com.github.javaparser.ast.body.MethodDeclaration;
+import com.github.javaparser.ast.expr.VariableDeclarationExpr;
 import com.github.javaparser.ast.body.VariableDeclarator;
 import com.github.javaparser.ast.visitor.VoidVisitorAdapter;
 import main.Driver;
@@ -10,33 +11,36 @@ import main.Driver;
 import java.util.HashMap;
 import java.util.Map;
 
-public class FieldDeclarationSmellVisitor extends VoidVisitorAdapter<Object> {
+public class FieldDeclarationSmellVisitor extends VoidVisitorAdapter<Void> {
 
     private Map<String, Node> declaredVariables = new HashMap<>();
 
     @Override
-    public void visit(FieldDeclaration n, Object args) {
-        super.visit(n, args);
+    public void visit(FieldDeclaration n, Void args) {
         for (VariableDeclarator v : n.getVariables()) {
             checkForShadowing(v);
         }
+        super.visit(n, args);
     }
 
     @Override
-    public void visit(MethodDeclaration n, Object args) {
+    public void visit(MethodDeclaration n, Void args) {
+        declaredVariables.clear();
         super.visit(n, args);
-        for (Node child : n.getChildNodes()) {
-            if (child instanceof VariableDeclarator) {
-                VariableDeclarator v = (VariableDeclarator) child;
-                checkForShadowing(v);
-            }
+    }
+
+    @Override
+    public void visit(VariableDeclarationExpr n, Void args) {
+        for (VariableDeclarator v : n.getVariables()) {
+            checkForShadowing(v);
         }
+        super.visit(n, args);
     }
 
     private void checkForShadowing(VariableDeclarator v) {
         String name = v.getNameAsString();
         if (declaredVariables.containsKey(name)) {
-            System.out.println("Variable '" + name + "' hides a declaration at a higher level");
+            Driver.smellyCodeFound("Variable '" + name + "' hides a declaration at a higher level.");
         } else {
             declaredVariables.put(name, v);
         }
